51f65eca1902fe27d5f4c6a030fffc8ef077d28a
RobertoZefi <zefi.roberto2001@gmail.com>
2023-02-14 16:01:13 +0100

    FEAT milestone 1 done

---
+++ app.js
@@ -0,0 +1,49 @@
+const team = [
+    {
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO',
+        foto: 'img'
+    },
+
+    {
+        nome: 'Angela Caroll',
+        ruolo: 'Chief Editor',
+        foto: 'img'
+    },
+
+    {
+        nome: 'Walter Gordon',
+        ruolo: 'Founder & CEO',
+        foto: 'img'
+    },
+    
+    {
+        nome: 'Angela Lopez',
+        ruolo: 'Social Media Manager',
+        foto: 'img'
+    },
+
+    {
+        nome: 'Scott Estrada',
+        ruolo: 'Developer',
+        foto: 'img'
+    },
+
+    {
+        nome: 'Barbara Ramos',
+        ruolo: 'Graphic Designer',
+        foto: 'img'
+    }
+]
+
+for (let key in team){
+    let teamPerson = team[key]
+    console.log(teamPerson)
+
+    let nome = teamPerson.nome
+    let ruolo = teamPerson.ruolo
+    let foto = teamPerson.foto
+    console.log(nome, ruolo, foto)
+
+    
+}
