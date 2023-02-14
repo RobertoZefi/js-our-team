8bb4ce494d9ef0b4f0c72bfcc1781e0e3e41e52c
RobertoZefi <zefi.roberto2001@gmail.com>
2023-02-14 18:03:33 +0100

    FEAT bonus done

--- app.js
+++ app.js
@@ -1,38 +1,47 @@
+let img = [
+    './img/wayne-barnett-founder-ceo.jpg',
+    './img/angela-caroll-chief-editor.jpg',
+    './img/walter-gordon-office-manager.jpg',
+    './img/angela-lopez-social-media-manager.jpg',
+    './img/scott-estrada-developer.jpg',
+    './img/barbara-ramos-graphic-designer.jpg',
+]
+
 const team = [
     {
         nome: 'Wayne Barnett',
         ruolo: 'Founder & CEO',
-        foto: 'img'
+        foto: `<img src="${img[0]}" alt="">`
     },
 
     {
         nome: 'Angela Caroll',
         ruolo: 'Chief Editor',
-        foto: 'img'
+        foto: `<img src="${img[1]}" alt="">`
     },
 
     {
         nome: 'Walter Gordon',
         ruolo: 'Founder & CEO',
-        foto: 'img'
+        foto: `<img src="${img[2]}" alt="">`
     },
     
     {
         nome: 'Angela Lopez',
         ruolo: 'Social Media Manager',
-        foto: 'img'
+        foto: `<img src="${img[3]}" alt="">`
     },
 
     {
         nome: 'Scott Estrada',
         ruolo: 'Developer',
-        foto: 'img'
+        foto: `<img src="${img[4]}" alt="">`
     },
 
     {
         nome: 'Barbara Ramos',
         ruolo: 'Graphic Designer',
-        foto: 'img'
+        foto: `<img src="${img[5]}" alt="">`
     }
 ]
 
@@ -48,8 +57,16 @@ for (let key in team){
     console.log(nome, ruolo, foto)
 
     let string = `
-        <div>${nome}, ${ruolo}, ${foto}</div>
+        <div class="card">
+            ${foto} 
+            <div class="card__description">
+                <h2 class="name">${nome} </h2>
+                <p class="ruolo">${ruolo}</p>
+            </div>
+        </div>
     `
-
+    console.log(string)
     divString.innerHTML += string
 }
+
+
